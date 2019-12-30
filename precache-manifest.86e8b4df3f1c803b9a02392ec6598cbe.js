bc459a7c43da93142df89ae3bf73c428333d5487
sensenye <100349203@alumnos.uc3m.es>
2019-12-30 23:07:38 +0100

    Updates

---
+++ precache-manifest.86e8b4df3f1c803b9a02392ec6598cbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2367371f897b87b4cfb4be26976e2333",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "1a509991dd12b46c0d41",
+    "url": "/WeatherApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ce964b1b2e4c1b335c83",
+    "url": "/WeatherApp/static/js/2.e35a7f8b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/WeatherApp/static/js/2.e35a7f8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a509991dd12b46c0d41",
+    "url": "/WeatherApp/static/js/main.567b77da.chunk.js"
+  },
+  {
+    "revision": "577b148b4a1231122a85",
+    "url": "/WeatherApp/static/js/runtime-main.45fa1ade.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WeatherApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
